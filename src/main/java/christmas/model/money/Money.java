5d7d7d9ce3f16d9b0f2e12d46f51847fc687ba4a
Bosub Kim <grapeflavoredapple@gmail.com>
2023-11-12 19:11:38 +0900

    feat: Money 정의
    금액을 관리하는 Money 클래스 정의

---
+++ src/main/java/christmas/model/money/Money.java
@@ -0,0 +1,26 @@
+package christmas.model.money;
+
+public class Money {
+
+    private int amount;
+
+    private Money(int amount) {
+        this.amount = amount;
+    }
+
+    public static Money of(int amount) {
+        validateMoney(amount);
+        return new Money(amount);
+    }
+
+    public void addAmount(int amount) {
+        validateMoney(amount);
+        this.amount += amount;
+    }
+
+    public static void validateMoney(int amount) {
+        if (amount < 0) {
+            throw new IllegalArgumentException();
+        }
+    }
+}
