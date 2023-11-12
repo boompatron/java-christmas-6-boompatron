53f98f033b957659a66d4540b71bf70f945818c9
Bosub Kim <grapeflavoredapple@gmail.com>
2023-11-12 23:45:32 +0900

    feat: 증정 이벤트 상수 정의

---
+++ src/main/java/christmas/model/event/GiveawayEventConstant.java
@@ -0,0 +1,32 @@
+package christmas.model.event;
+
+import static christmas.model.menu.Menu.샴페인;
+
+import christmas.model.menu.Menu;
+
+public enum GiveawayEventConstant {
+
+    GIVEAWAY_EVENT("증정 이벤트", 120000, 샴페인),;
+
+    private final String name;
+    private final int threshold;
+    private final Menu eventMenu;
+
+    GiveawayEventConstant(String name, int threshold, Menu eventMenu) {
+        this.name = name;
+        this.threshold = threshold;
+        this.eventMenu = eventMenu;
+    }
+
+    public int getThreshold() {
+        return threshold;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public Menu getEventMenu() {
+        return eventMenu;
+    }
+}
