c49fe146c31506fdca1530fd4fd759e7bcdfcc67
Bosub Kim <grapeflavoredapple@gmail.com>
2023-11-12 19:10:54 +0900

    feat: 메뉴 상수 정의
    - 메뉴 이름을 통해 상수 객체를 생성할 수 있도록 설정
    - 메뉴 메뉴는 타입과, 가격을 가지고 있음

---
+++ src/main/java/christmas/model/menu/Menu.java
@@ -0,0 +1,40 @@
+package christmas.model.menu;
+
+import static christmas.model.menu.MenuType.APPETIZER;
+import static christmas.model.menu.MenuType.DESSERT;
+import static christmas.model.menu.MenuType.DRINK;
+import static christmas.model.menu.MenuType.MAIN;
+
+public enum Menu {
+    양송이수프(APPETIZER, 6000),
+    타파스(APPETIZER, 5500),
+    시저샐러드(APPETIZER, 8000),
+
+    티본스테이크(MAIN, 55000),
+    바비큐립(MAIN, 54000),
+    해산물파스타(MAIN, 35000),
+    크리스마스파트타(MAIN, 25000),
+
+    초코케이크(DESSERT, 15000),
+    아이스크림(DESSERT, 5000),
+
+    제로콜라(DRINK, 3000),
+    레드와인(DRINK, 60000),
+    샴페인(DRINK, 25000);
+
+    private final MenuType menuType;
+    private final int price;
+
+    Menu(MenuType menuType, int price) {
+        this.menuType = menuType;
+        this.price = price;
+    }
+
+    public MenuType getMenuType() {
+        return menuType;
+    }
+
+    public int getPrice() {
+        return price;
+    }
+}
