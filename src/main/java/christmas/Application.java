7d06147174164b2ea1d85391d4772ea33aa3e216
Bosub Kim <grapeflavoredapple@gmail.com>
2023-11-13 02:23:18 +0900

    feat: Application 수정
    프로그램이 수행될 수 있도록 수정

--- src/main/java/christmas/Application.java
+++ src/main/java/christmas/Application.java
@@ -1,7 +1,16 @@
 package christmas;
 
+import christmas.io.IOManager;
+import christmas.io.InputView;
+import christmas.io.OutputView;
+import christmas.model.controller.Controller;
+import christmas.service.DiscountEventService;
+import christmas.service.GiveawayEventService;
+
 public class Application {
     public static void main(String[] args) {
-        // TODO: 프로그램 구현
+        IOManager ioManager = new IOManager(new InputView(), new OutputView());
+        Controller controller = new Controller(ioManager, DiscountEventService.of(), GiveawayEventService.of());
+        controller.run();
     }
 }
