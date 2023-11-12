14d6b09c4ad63845f3943bfda812ed2e1e75fb35
Bosub Kim <grapeflavoredapple@gmail.com>
2023-11-13 00:23:14 +0900

    feat: OutputView 구현
    - 출력을 담당하는 OutputView 클래스를 구현함

--- src/main/java/christmas/io/OutputView.java
+++ src/main/java/christmas/io/OutputView.java
@@ -22,4 +22,39 @@ public class OutputView {
     public void printShowEventMessage() {
         System.out.println(IOConstant.SHOW_EVENT_MESSAGE.getValue());
     }
+
+    public void printAllMenus(String menus) {
+        System.out.println(IOConstant.ORDERED_MENU.getValue());
+        System.out.println(menus);
+    }
+
+    public void printTotalPriceBeforeDiscount(String totalPrice) {
+        System.out.println(IOConstant.AMOUNT_BEFORE_DISCOUNT.getValue());
+        System.out.println(totalPrice);
+    }
+
+    public void printGiveawayEvent(String giveawayMenu) {
+        System.out.println(IOConstant.GIVEAWAY_MENU.getValue());
+        System.out.println(giveawayMenu);
+    }
+
+    public void printBenefitsDetail(String benefitsDetail) {
+        System.out.println(IOConstant.BENEFITS_DETAIL.getValue());
+        System.out.println(benefitsDetail);
+    }
+
+    public void printTotalBenefitsAmount(String totalBenefitsAmount) {
+        System.out.println(IOConstant.TOTAL_BENEFITS_AMOUNT.getValue());
+        System.out.println(totalBenefitsAmount);
+    }
+
+    public void printTotalPriceAfterDiscount(String totalPriceAfterDiscount) {
+        System.out.println(IOConstant.AMOUNT_AFTER_DISCOUNT.getValue());
+        System.out.println(totalPriceAfterDiscount);
+    }
+
+    public void printEventBadge(String eventBadge) {
+        System.out.println(IOConstant.EVENT_BADGE.getValue());
+        System.out.println(eventBadge);
+    }
 }
